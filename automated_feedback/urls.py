d21f68af819f6b87bc06841eed9e9882d42729e0
anwar <anwaruuo88@gmail.com>
2020-04-30 00:51:24 +0100

    Added new files

--- automated_feedback/urls.py
+++ automated_feedback/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.http import HttpResponse
 urlpatterns = [
+    path('health', lambda request : HttpResponse('okay')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
